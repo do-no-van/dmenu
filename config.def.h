ce69b53250a0e3fe7c13501c01bb8d52c62b52d3
do-no-van <47617272696e@gmail.com>
2022-01-26 16:32:32 -0600

    Add emoji font

--- config.def.h
+++ config.def.h
@@ -6,6 +6,7 @@ static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"FiraCode Nerd Font:pixelsize=13:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=12:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
